0d3895f9a097db59fb6554ba03357c1091e1c087
dylpckl <dylanjbsmith@gmail.com>
2023-08-15 16:50:35 -0400

    build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,24 +95,26 @@ export default function Home() {
         <div className="flex flex-col gap-6 mt-6 text-slate-300">
           <p className="">
             I&apos;m Dylan, a UI designer & developer from Long Island, New
-            York. Designing for the web has been a big part of my life going all
-            the way back to the summer of 2005, when I snagged myself a copy of
-            Photoshop CS and became obsessed with creating forum signatures.
+            York. In the summer of 2005 I snagged myself a copy of Photoshop CS
+            and became obsessed with creating forum signatures. Designing for
+            the web has been a big part of my life ever since.
           </p>
           <p>
-            I'm currently the lead UI/UX Designer for{" "}
+            I&apos;m currently the lead UI/UX Designer for{" "}
             <a
               className="font-medium text-slate-100 hover:text-teal-300 hover:underline"
               href="https://multidataservices.com/"
             >
-              New York's premier property management software solution
+              New York&apos;s premier property management software solution
             </a>
             , where my focus is designing an enterprise application that&apos;s
-            easy to use, nice to look at, and that gets out of the user&apos;s way.
+            easy to use, nice to look at, and that gets out of the user&apos;s
+            way.
           </p>
           <p>
-            When I&apos;m not at my computer, I can usually be found hanging out with my
-            wife & daughter, hiking in the mountains, or telling stories around a table with friends.
+            When I&apos;m not at my computer, I can usually be found hanging out
+            with my wife & daughter, hiking in the mountains, or telling stories
+            around a table with friends.
           </p>
         </div>
 
