4fcb20b2a2b0301ff2f1a9a171e442fe4cad7539
dylpckl <dylanjbsmith@gmail.com>
2023-09-05 08:39:32 -0400

    MDX

--- src/app/projects/[slug]/page.js
+++ src/app/projects/[slug]/page.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import { getArticleFromSlug } from '@/lib/mdx';
 
-const page = ({ params }) => {
+import { getArticleFromSlug, getAllArticles } from '@/lib/mdx';
+import { MDXRemote } from 'next-mdx-remote/rsc'
+import { useMDXComponents } from '@/app/mdx-components';
+
+// export const generateStaticParams = async () => {
+//     const projects = getAllArticles();
+//     return projects.map((project) => ({
+//         slug: project.slug,
+//     }));
+// };
+
+export async function generateMetadata({ params }) {
     const { slug } = params;
-    const project = getArticleFromSlug(slug)
-    console.log(project)
-    return (
-        <div>page</div>
-    )
+    const project = getArticleFromSlug(params);
+
+    // return {
+    //     title: blog.frontMatter.title,
+    //     description: blog.frontMatter.description,
+    // }
 }
 
-export default page
+export default async function Project({ params }) {
+    const { slug } = params;
+    const project = await getArticleFromSlug(slug)
+    console.log("HERE", project)
+    const MDXContent = useMDXComponents(project.content)
+    return (
+        <article className='prose prose-slate dark:prose-invert prose-sm max-w-none'>
+            <h1>{project.frontmatter.excerpt}</h1>
+            {/* <MDXRemote source={project.content} /> */}
+            <MDXContent />
+        </article>
+    )
+}
